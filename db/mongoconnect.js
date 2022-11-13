4aced1675cc57ed57bf0d2609c2b115c8607aab3
Netanel Alroy <netanelal100@gmail.com>
2022-11-13 16:08:01 +0200

    add all

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -5,7 +5,7 @@ const {config} = require("../config/secret");
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.rnllwmz.mongodb.net/project`);
+  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.rnllwmz.mongodb.net/black22`);
   console.log("mongo connect project atlas")
 
 }
